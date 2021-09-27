71443df8abd88975495030a3733edd674d17dd0d
Savannah Mann <mannnnnn@users.noreply.github.com>
2021-09-26 21:05:46 -0500

    Its beginning to look a lot like pinball
    just... sans the mechanics. It looks kinda fun tho

---
+++ Cursed Pinball/Assets/ChimeraController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChimeraController : MonoBehaviour
+{
+    public Animator LFlipper;
+    public Animator RFlipper;
+    public GameObject Ball;
+    public Transform RespawnArea;
+
+    private Rigidbody2D ballRB;
+
+    [Header("Settings To Fiddle With")]
+    public float launchSpeed = 1;
+
+    public static ChimeraController GetInstance()
+    {
+        return FindObjectsOfType<ChimeraController>(true)[0];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRB = Ball.GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool leftPressed = Input.GetKey(KeyCode.LeftArrow);
+        bool rightPressed = Input.GetKey(KeyCode.RightArrow);
+        LFlipper.SetBool("FlipperHold", leftPressed);
+        RFlipper.SetBool("FlipperHold", rightPressed);
+    }
+
+    public void ResetBall()
+    {
+        Ball.transform.position = RespawnArea.position;
+    }
+
+    public void LaunchBall()
+    {
+        ballRB.velocity += new Vector2(0, launchSpeed);
+    }
+}
